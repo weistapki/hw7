31d5ad4c9cb792b07cc8c97b2b324e3ccb5953fc
weistapki <weistapki@gmail.com>
2023-03-28 16:18:12 +0200

    init commit

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -3,5 +3,7 @@ package task1;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        String examp = " ";
+        System.out.println("Example  : " + solution.isPalindrome(examp));
     }
 }
